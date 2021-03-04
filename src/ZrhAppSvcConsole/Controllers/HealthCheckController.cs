bf3a1ddc8e7bb34e942a43682d47689027f78fd4
AutoExtractor <crgarcia@live.com.ar>
2021-03-04 11:57:14 +0100

    add a health check endpoint

---
+++ src/ZrhAppSvcConsole/Controllers/HealthCheckController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZrhAppSvcConsoleWeb.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Healthy!");
+        }
+    }
+}
